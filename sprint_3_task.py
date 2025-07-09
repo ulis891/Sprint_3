0543e008f4ff2d5c061200bb470c738fa7e21715
Aleksandr Kostromin <ulis89@yandex.ru>
2025-07-09 13:23:21 +0300

    add task_1

---
+++ sprint_3_task.py
@@ -0,0 +1,17 @@
+import datetime
+
+class OnlineSalesRegisterCollector:
+
+    def __init__(self):
+        self.__name_items = []
+        self.__number_items = 0
+        self.__item_price = {'чипсы': 50, 'кола': 100, 'печенье': 45, 'молоко': 55, 'кефир': 70}
+        self.__tax_rate = {'чипсы': 20, 'кола': 20, 'печенье': 20, 'молоко': 10, 'кефир': 10}
+
+    @property
+    def get_name_items(self):
+         return self.__name_items
+    
+    @property
+    def get_number_items(self):
+        return self.__number_items
