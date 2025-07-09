2c09148e2aa99d7d45166e79e1cd3c8075a964b8
Aleksandr Kostromin <ulis89@yandex.ru>
2025-07-09 16:02:01 +0300

    add task_2

--- sprint_3_task.py
+++ sprint_3_task.py
@@ -15,3 +15,12 @@ class OnlineSalesRegisterCollector:
     @property
     def get_number_items(self):
         return self.__number_items
+    
+    def add_item_to_cheque(self, name):
+        if len(name) == 0 or len(name) > 40:
+            raise ValueError('Нельзя добавить товар, если в его названии нет символов или их больше 40')
+        elif name not in self.__item_price:
+            raise ValueError('Позиция отсутствует в товарном справочнике')
+        else:
+            self.__name_items.append(name)
+            self.__number_items += 1
